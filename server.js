e5ac0b85fc7f6e7bccc2c7c9f0753e34c953a80d
mec64 <masonecrane@gmail.com>
2015-05-11 15:02:22 -0600

    emit works on server and client side, as well as capturing values from form inputs without reloading page

--- server.js
+++ server.js
@@ -20,4 +20,10 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket){
     console.log('Using sockets!');
     console.log(socket.id);
+    socket.on('new_form', function(data){
+        console.log(data);
+        var random_number = Math.ceil(Math.random() * 1000);
+        console.log(random_number);
+        socket.emit('server_response', {name: data.name, location: data.location, language: data.language, comment: data.comment, random_number: random_number});
+    });
 });
